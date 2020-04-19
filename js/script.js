775328fd4aeeab33dda077e1a46ce0dfc29c343d
Ryan Skog <ryan.skog@outlook.com>
2020-04-19 16:16:34 -0700

    Started the framework for JS/HTML.  Added functions for array manipulation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 let listQuestion = document.querySelector("#list-question");
 let listAnswers = document.querySelector("#list-answers");
+let submitButton = document.querySelector("#submit");
+let announcement = document.querySelector("#announcement");
+let explanation = document.querySelector("#explanation");
+let startButton = document.querySelector("#start")
 
 
 let questions = [
@@ -55,9 +59,17 @@ let questions = [
     }
 
 ]
-
 let randomQ = questions[Math.floor(Math.random() * questions.length)];
 let ansBank = [];
+
+listQuestion.textContent = "";
+listAnswers.textContent = "";
+announcement.textContent = "Coding Quiz"
+explanation.textContent = "Welcome to the coding quiz.  You will get a series of 10 questions, and have 10 seconds to answer each question. Scoring is awarded by how fast each question is answered. There is a penalty for incorrect answers, so review all choices before submitting.  Click the button below to start the quiz."
+
+function startQuiz() {
+
+}
 
 // Puts all answers in an array for random order output
 function answerArr() {
@@ -67,21 +79,48 @@ function answerArr() {
     }
 }
 
-// Puts all answers into HTML list
+// Puts all answers into HTML 
 function listAns() {
 
     answerArr();
+    shuffle(ansBank);
 
     listQuestion.textContent = randomQ.question;
 
     for (i = 0; i < ansBank.length; i++) {
+
         let li = document.createElement("li");
-        li.innerHTML = ansBank[i];
+        let input = document.createElement("input");
+        let label = document.createElement("label");
+
+        input.setAttribute("type", "radio");
+        input.setAttribute("name", "chooseAns");    //Makes only one radio button selectable at a time
+        input.setAttribute("id", i);                //Assigns ID of answer to match array ID
+        label.setAttribute("for", input.id);
+
+        label.innerHTML = ansBank[i];
+
         listAnswers.appendChild(li);
+        li.append(input, label);
     }
 }
 
-listAns();
+function nextQuestion() {
+
+}
+
+//Code borrowed from http://javascript.info/task/shuffle, Fisher-Yates shuffle
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+submitButton.addEventListener("click", nextQuestion);
+startButton.addEventListener("click", startQuiz);
+
+//listAns();
 
 console.log(randomQ.question);
 console.log(ansBank);
